c95f39293e0d95430535e2b648b1b1f098a9ff42
Павел Ажель <p.azhel@andersenlab.com>
2024-09-23 19:58:06 +0200

    supertest

---
+++ searcher/src/test/java/com/example/citiessearcher/searcher/presentation/SuperTest.kt
@@ -0,0 +1,12 @@
+package com.example.citiessearcher.searcher.presentation
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class SuperTest {
+
+    @Test
+    fun superTest() {
+        assertEquals(2+2, 5)
+    }
+}
